a4c7db3677c9ca03c0e525bc66f39703951ec5dd
michele <michele.deliso@gmail.com>
2023-06-01 17:08:10 +0200

    feat: use of redux-toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./root-reducer";
-import thunk from "redux-thunk";
 import logger from "redux-logger";
 /* import { compose, createStore, applyMiddleware } from "redux";
 
@@ -19,11 +18,15 @@ const persistentReducer = persistReducer(persistConfig, rootReducer) */
 
 // logger allows us to see what the state is, what the action is and how the state looks after the action
 // middleware runs before the action hits the reducer
-const middlewares = [logger, thunk /* middleware2, middleware3, etc */];
+const middlewares = [logger /* middleware2, middleware3, etc */];
 // in order to work, middlewares must be passed as argument in the 'compose' method
 //const composedEnhancers = compose(applyMiddleware(...middlewares));
 
 export const store = configureStore({
-    reducer: rootReducer,
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(middlewares),
 });
 // export const persistorStore = persistStore(store)
